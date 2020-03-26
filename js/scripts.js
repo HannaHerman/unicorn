e91248f391d2849c0255ea862f75310542194ec7
HannaHerman <mrs.hanna.herman@gmail.com>
2020-03-26 23:41:12 +0300

    v1.10 - fixed sliders navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,86 @@
+// featured articles
 $('.featured-articles__slider').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    infinite: false
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  infinite: false,
+  prevArrow: '.featured-articles__slick-prev',
+  nextArrow: '.featured-articles__slick-next',
   });
+  $('.featured-articles__slider').on('afterChange', function(){
+    console.log($('.featured-articles__slider').slick('slickCurrentSlide'));
+    var currentSlide = $('.featured-articles__slider').slick('slickCurrentSlide');
+  if(currentSlide==0)
+  {
+     $('.featured-articles__slick-prev').hide();
+     $('.featured-articles__slick-next').show();
+  }
+  else if(currentSlide==3)
+  {
+  $('.featured-articles__slick-next').hide();
+  $('.featured-articles__slick-prev').show();
+  }
+  
+  if(currentSlide>0 && currentSlide<3)
+  {
+     $('.featured-articles__slick-prev').show();
+     $('.featured-articles__slick-next').show();
+  }
+  });
+  $(document).ready(function(){
+  var currentSlide = $('.featured-articles__slider').slick('slickCurrentSlide');
+  if(currentSlide==0)
+  {
+     $('.featured-articles__slick-prev').hide();
+  }
+  else if(currentSlide==3)
+  {
+  $('.featured-articles__slick-next').hide();
+  }
+  });
+  
+  // clients
   $('.clients__slider').slick({
     slidesToShow: 6,
     slidesToScroll: 1,
+    adaptiveHeight: true,
     variableWidth: true,
+    infinite: false,
+  prevArrow: '.clients__slick-prev',
+  nextArrow: '.clients__slick-next',
+  });
+  $('.clients__slider').on('afterChange', function(){
+    console.log($('.clients__slider').slick('slickCurrentSlide'));
+    var currentSlide = $('.clients__slider').slick('slickCurrentSlide');
+  if(currentSlide==0)
+  {
+     $('.clients__slick-prev').hide();
+     $('.clients__slick-next').show();
+  }
+  else if(currentSlide==6)
+  {
+  $('.clients__slick-next').hide();
+  $('.clients__slick-prev').show();
+  }
+  
+  if(currentSlide>0 && currentSlide<6)
+  {
+     $('.clients__slick-prev').show();
+     $('.clients__slick-next').show();
+  }
+  });
+  $(document).ready(function(){
+  var currentSlide = $('.clients__slider').slick('slickCurrentSlide');
+  if(currentSlide==0)
+  {
+     $('.clients__slick-prev').hide();
+  }
+  else if(currentSlide==6)
+  {
+  $('.clients__slick-next').hide();
+  }
   });
   
+
 $('.filtering').slick({
   centerMode: true,
   centerPadding: '60px',
@@ -48,8 +120,44 @@ $('.js-filter').on('click', function(){
     filtered = false;
   }
 });
-$('.blog__slider .container').slick({
+
+// blog
+$('.blog-articles__slider').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
-  adaptiveHeight: true
+  adaptiveHeight: true,
+  infinite: false,
+  prevArrow: '.blog-articles__slick-prev',
+  nextArrow: '.blog-articles__slick-next',
+});
+$('.blog-articles__slider').on('afterChange', function(){
+  console.log($('.blog-articles__slider').slick('slickCurrentSlide'));
+  var currentSlide = $('.blog-articles__slider').slick('slickCurrentSlide');
+if(currentSlide==0)
+{
+   $('.blog-articles__slick-prev').hide();
+   $('.blog-articles__slick-next').show();
+}
+else if(currentSlide==3)
+{
+$('.blog-articles__slick-next').hide();
+$('.blog-articles__slick-prev').show();
+}
+
+if(currentSlide>0 && currentSlide<3)
+{
+   $('.blog-articles__slick-prev').show();
+   $('.blog-articles__slick-next').show();
+}
+});
+$(document).ready(function(){
+var currentSlide = $('.blog-articles__slider').slick('slickCurrentSlide');
+if(currentSlide==0)
+{
+   $('.blog-articles__slick-prev').hide();
+}
+else if(currentSlide==3)
+{
+$('.blog-articles__slick-next').hide();
+}
 });
