3f7cf3610747b15b14b2793d7a7d773af00b1ce0
HannaHerman <mrs.hanna.herman@gmail.com>
2020-04-03 18:57:47 +0300

    v2.1 - updated view-all page

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    return gulp.src('sass/read-rest.scss')
+    return gulp.src('sass/view-all.scss')
         .pipe(sass())
         .pipe(sass({
             includePaths: require('node-normalize-scss').with('other/path', 'another/path')
